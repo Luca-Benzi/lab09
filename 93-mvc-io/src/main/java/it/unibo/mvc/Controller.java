6324a21f50890298f6765efaa0c35e9155dc4980
Luca-Benzi <luca.benzi2@studio.unibo.it>
2024-11-18 15:03:39 +0100

    inizio es.93

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,12 @@
 package it.unibo.mvc;
+import java.util.List;
 
 /**
  *
  */
 public interface Controller {
-
+    void setString(final String string);
+    String getString();
+    List<String> getHistory();
+    void printString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
 
+    private List<String> history = new LinkedList<>();
+    @Override
+    public void setString(String string) {
+        
+
+    @Override
+    public String getString() {
+        
+    }
+
+    @Override
+    public List<String> getHistory() {
+        
+    }
+
+    @Override
+    public void printString() {
+        
+    }
+    
 }
